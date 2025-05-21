9a84f73fba7480dd73f4a73a2efbeca117cc394d
jimmyhan <jimmyh@ensemble.com>
2025-05-20 17:49:25 -0700

    fix test issues

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import * as core from '@actions/core'
 import { createComment } from './create-comment.js'
+import { wait } from './wait.js'
 
 /**
  * The main function for the action.
@@ -11,6 +12,15 @@ export async function run(): Promise<void> {
     const commentId = await createComment()
     // Set outputs for other workflow steps to use
     core.setOutput('commentId', commentId)
+
+    // Log the current timestamp, wait, then log the new timestamp
+    core.debug(new Date().toTimeString())
+    const ms = core.getInput('milliseconds')
+    await wait(parseInt(ms, 10))
+    core.debug(new Date().toTimeString())
+
+    // Set outputs for other workflow steps to use
+    core.setOutput('time', new Date().toTimeString())
   } catch (error) {
     // Fail the workflow run if an error occurs
     if (error instanceof Error) core.setFailed(error.message)
